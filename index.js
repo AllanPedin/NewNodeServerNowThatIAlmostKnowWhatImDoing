33d004abc061d9035b2a75bc4e53a2b2c5939212
Allan Pedin <apedin@ddig.com>
2018-06-28 12:49:55 -0400

    took out one of the routes

--- index.js
+++ index.js
@@ -11,9 +11,18 @@ mongoose.connect('mongodb://localhost/my_db');
 var upload = multer();
 var app = express();
 
-app.get('/SkillsFramework', function(req, res){
-	   res.render('SkillsFrameWorkLandingPage');
+//
+var personSchema = mongoose.Schema({
+	   name: String,
+	   age: Number,
+	   nationality: String
 	});
+var Person = mongoose.model("Person", personSchema);
+//
+
+//app.get('/SkillsFramework', function(req, res){
+//	   res.render('SkillsFrameWorkLandingPage');
+//	});
 
 app.set('view engine', 'jade');
 app.set('views', './views');
