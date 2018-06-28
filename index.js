94231bca709a318b1737dd337ae5de6dd9ccbf78
Allan Pedin <apedin@ddig.com>
2018-06-28 10:50:17 -0400

    shell of a shell of a basic functionality

--- index.js
+++ index.js
@@ -2,20 +2,40 @@
  * 
  */
 var express = require('express');
+var bodyParser =require('body-parser');
+var multer = require('multer');
 
+var upload = multer();
 var app = express();
 
+app.get('/SkillsFramework', function(req, res){
+	   res.render('SkillsFrameWorkLandingPage');
+	});
+
+app.set('view engine', 'jade');
+app.set('views', './views');
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array()); 
+app.use(express.static('public'));
 
 app.use(function(req, res, next){
 	   console.log("A new request received at " + Date.now());
 	   next();
 });
-//routes
-app.get('/SkillsFramework', function(req, res){
-	   res.send('Things');
-	});
-//routes
 
+//////////////////////
+app.get('/SkillsFramework', function(req, res){
+	   res.render('SkillsFrameWorkLandingPage');
+	});
+
+app.post('/SkillsFramework', function(req, res){
+	   console.log(req.body);
+	   
+	   res.send("Created Your Person!(jk nothing happened)");
+	});
+///////////////////////
 app.use(function(req, res){
 	   console.log('Request dealt with at '+ Date.now());
 	});
